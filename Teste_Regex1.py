0a3c3b1b5cbf038326992a2aa3e1a963cc7e2b7c
Felipe Souza Moreira <felipemoreira2003@gmail.com>
2025-04-11 18:48:01 -0300

    Primeiro envio

--- Teste_Regex1.py
+++ Teste_Regex1.py
@@ -17,11 +17,11 @@ studies_iuids = re.findall(padrao_studies, texto)
 
 # Verificando se há exames encontrados
 if datas_exames and empresas_ids and studies_iuids:
-    print("\n✅ Dados extraídos:")
+    print("\n Dados extraídos:")
     for i in range(len(datas_exames)):
         print(f"\n🔹 Exame {i+1}:")
-        print(f"📅 Data do Exame: {datas_exames[i]}")
-        print(f"🏢 Empresa ID: {empresas_ids[i]}")
-        print(f"🔍 Studies IUID: {studies_iuids[i]}")
+        print(f"Data do Exame: {datas_exames[i]}")
+        print(f"Empresa ID: {empresas_ids[i]}")
+        print(f"Studies IUID: {studies_iuids[i]}")
 else:
-    print("\n⚠ Nenhum exame encontrado! Verifique se os dados foram inseridos corretamente.")
+    print("\n Nenhum exame encontrado! Verifique se os dados foram inseridos corretamente.")
